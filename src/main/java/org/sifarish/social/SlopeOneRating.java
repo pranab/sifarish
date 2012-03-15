19c69f8844e015bfee2d4619c87d9202f5469cad
Pranab Ghosh <pkghosh99@gmail.com>
2012-03-14 23:43:14 -0700

    slope one recommender

--- src/main/java/org/sifarish/social/SlopeOneRating.java
+++ src/main/java/org/sifarish/social/SlopeOneRating.java
@@ -21,14 +21,12 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
