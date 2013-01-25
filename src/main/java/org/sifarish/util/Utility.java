16b245d4e851169acb2bdd106c87c5b66484baf6
Anthony Yee <smyee@yahoo.com>
2013-01-25 00:02:01 -0800

    Added support to load configuration from S3

--- src/main/java/org/sifarish/util/Utility.java
+++ src/main/java/org/sifarish/util/Utility.java
@@ -18,9 +18,18 @@
 package org.sifarish.util;
 
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.hadoop.conf.Configuration;
+
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 /**
  * Utility
@@ -28,13 +37,35 @@ import org.apache.hadoop.conf.Configuration;
  *
  */
 public class Utility {
+	private static final String S3_PREFIX = "s3n:";
+	private static Pattern s3pattern = Pattern.compile("s3n:/+([^/]+)/+(.*)");
+    static AmazonS3 s3 = null;
+	static {
+		try {	
+			s3 = new AmazonS3Client(new PropertiesCredentials(Utility.class.getResourceAsStream("AwsCredentials.properties")));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
     public static void setConfiguration(Configuration conf) throws Exception{
         String confFilePath = conf.get("conf.path");
         if (null != confFilePath){
-            FileInputStream fis = new FileInputStream(confFilePath);
+        	InputStream is = null;
+        	if (confFilePath.startsWith(S3_PREFIX)) { 
+        		Matcher matcher = s3pattern.matcher(confFilePath);
+        		matcher.matches();
+        		String bucket = matcher.group(1);
+        		String key = matcher.group(2);
+        		S3Object object = s3.getObject(new GetObjectRequest(bucket, key));
+                is = object.getObjectContent();
+        	}
+        	else {
+        		is = new FileInputStream(confFilePath);
+        	}
             Properties configProps = new Properties();
-            configProps.load(fis);
+            configProps.load(is);
 
             for (Object key : configProps.keySet()){
                 String keySt = key.toString();
