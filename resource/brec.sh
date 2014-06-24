bf3e5359272c430b6c355504dedce92b27862d5c
Pranab Ghosh <pkghosh99@gmail.com>
2014-06-23 22:14:44 -0700

    update tutorial

--- resource/brec.sh
+++ resource/brec.sh
@@ -14,6 +14,19 @@ PROP_FILE=/home/pranab/Projects/bin/sifarish/reco.properties
 HDFS_META_BASE_DIR=/user/pranab/meta/imra
 
 case "$1" in
+
+"genExplicitRating")  
+	echo "generating explicit rating data"
+	ruby ratings.rb $2 $3 $4  > $5
+    ;;
+
+"expExplicitRating")  
+	echo "exporting rating data to HDFS"
+	hadoop fs -rmr $HDFS_BASE_DIR/rate/*
+	hadoop fs -put $2 $HDFS_BASE_DIR/rate
+	hadoop fs -ls $HDFS_BASE_DIR/rate
+    ;;
+
 "expSchema")  
 	echo "exporting event mapping metadata to HDFS"
 	hadoop fs -rmr $HDFS_META_BASE_DIR/*
@@ -89,9 +102,9 @@ case "$1" in
 	hadoop fs -rmr $HDFS_BASE_DIR/stat/_SUCCESS	
 	;;
 	
-"copyRatingStat")	
-	echo "copying rating stat file to rating predictor input dir"
-	hadoop fs -cp $HDFS_BASE_DIR/stat/part-r-00000 $HDFS_BASE_DIR/simi/stat_ratings.txt
+"renameRatingStat")	
+	echo "renaming rating stat file"
+	hadoop fs -cp $HDFS_BASE_DIR/stat/part-r-00000 $HDFS_BASE_DIR/stat/$2
 	hadoop fs -ls $HDFS_BASE_DIR/simi
 	;;
 
@@ -104,7 +117,7 @@ case "$1" in
 "ratingPred")
 	echo "running MR for rating predictor"
 	CLASS_NAME=org.sifarish.common.UtilityPredictor
-	IN_PATH=$HDFS_BASE_DIR/crat,$HDFS_BASE_DIR/simi
+	IN_PATH=$HDFS_BASE_DIR/crat,$HDFS_BASE_DIR/stat,$HDFS_BASE_DIR/simi
 	OUT_PATH=$HDFS_BASE_DIR/utpr
 	echo "input $IN_PATH output $OUT_PATH"
 	hadoop fs -rmr $OUT_PATH
@@ -204,6 +217,21 @@ case "$1" in
 	hadoop fs -rmr $HDFS_BASE_DIR/utno/_logs
 	hadoop fs -rmr $HDFS_BASE_DIR/utno/_SUCCESS	
 	;;
+
+"sortByUser")	
+	JAR_NAME=/home/pranab/Projects/chombo/target/chombo-1.0.jar
+	CLASS_NAME=org.chombo.mr.TextSorter
+	echo "running TextSorter mr ro sort by userID"
+	IN_PATH=/user/pranab/reco/$2
+	OUT_PATH=/user/pranab/reco/usso
+	echo "input $IN_PATH output $OUT_PATH"
+	hadoop fs -rmr $OUT_PATH
+	echo "removed output dir $OUT_PATH"
+	hadoop jar $JAR_NAME  $CLASS_NAME -Dconf.path=$PROP_FILE  $IN_PATH  $OUT_PATH
+	hadoop fs -rmr $HDFS_BASE_DIR/usso/_logs
+	hadoop fs -rmr $HDFS_BASE_DIR/usso/_SUCCESS	
+	;;
+
 
 "itemPopularity")	
 	CLASS_NAME=org.chombo.mr.WeightedAverage
